e019d879578f0e11185c833101af3acd958c882b
Matthew D Webb <matt.d.webb@icloud.com>
2020-08-23 23:50:11 +0100

    feat: updates to serverless config to correct react app deploy

--- handler.js
+++ handler.js
@@ -5,14 +5,11 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Boom - Welcome to the Chess Centre!',
         input: event,
       },
       null,
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
