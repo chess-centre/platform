640905daa9f0f6d81121c006f148f043c25690a8
Matthew Webb <matt.d.webb@icloud.com>
2021-03-09 23:18:36 +0000

    mobile css tweaks for nav

--- chess-centre-app/src/pages/Home/LandingPage.js
+++ chess-centre-app/src/pages/Home/LandingPage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/img/logo.svg";
+import LogoWithName from "../../assets/img/logo-light-theme.png"
 import FooterLanding from "../../components/Footer/LandingFooter";
 
 const LandingPage = () => {
 
-  const [isExpanded, toggleExpansion] = React.useState(true);
+  const [isExpanded, toggleExpansion] = React.useState(false);
 
   return (
     <div>
@@ -37,7 +38,7 @@ const LandingPage = () => {
               <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
                 <div className="flex items-center justify-between w-full md:w-auto">
                   <a href="/" aria-label="Home">
-                    <img className="h-20 w-auto sm:h-10" src={Logo} alt="Logo" />
+                    <img className="object-center h-10 md:w-auto" src={Logo} alt="Logo" />
                   </a>
                   <div className={(isExpanded ? "" : "hidden") + "-mr-2 flex items-center md:hidden"}>
                     <button onClick={() => toggleExpansion(!isExpanded)} type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out" id="main-menu" aria-label="Main menu" aria-haspopup="true">
@@ -66,9 +67,7 @@ const LandingPage = () => {
             <div className="rounded-lg shadow-md">
               <div className="rounded-lg bg-white shadow-xs overflow-hidden" role="menu" aria-orientation="vertical" aria-labelledby="main-menu">
                 <div className="px-5 pt-4 flex items-center justify-between">
-                  <div>
-                    <img className="h-20 w-auto sm:h-10" src={Logo} alt="Logo" />
-                  </div>
+                    <img className="object-centre h-8 w-auto" src={LogoWithName} alt="Logo" />
                   <div className="-mr-2">
                     <button onClick={() => toggleExpansion(!isExpanded)} type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Close menu">
                       <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -78,14 +77,14 @@ const LandingPage = () => {
                   </div>
                 </div>
                 <div className="px-2 pt-2 pb-3">
-                  <Link className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem" to="/events">Events</Link>
-                  <Link className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem" to="/membership">Membership</Link>
-                  <Link className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem" to="/our-mission">Our Mission</Link>
+                  <Link className="text-centre sm:text-left block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-teal-900 hover:bg-orange-50 focus:outline-none focus:text-teal-900 focus:bg-orange-50 transition duration-150 ease-in-out" role="menuitem" to="/events">Events</Link>
+                  <Link className="text-centre sm:text-left mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-teal-900 hover:bg-orange-50 focus:outline-none focus:text-teal-900 focus:bg-orange-50 transition duration-150 ease-in-out" role="menuitem" to="/membership">Membership</Link>
+                  <Link className="text-centre sm:text-left mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-teal-900 hover:bg-orange-50 focus:outline-none focus:text-teal-900 focus:bg-orange-50 transition duration-150 ease-in-out" role="menuitem" to="/our-mission">Our Mission</Link>
                 </div>
                 <div>
-                  <Link className="block w-full px-5 py-3 text-center font-medium text-teal-600 bg-gray-50 hover:bg-gray-100 hover:text-teal-700 focus:outline-none focus:bg-gray-100 focus:text-teal-700 transition duration-150 ease-in-out" role="menuitem" to="/login">
+                  <Link className="block w-full px-5 py-3 text-center font-medium text-teal-900 bg-gray-50 hover:bg-gray-100 hover:text-teal-700 focus:outline-none focus:bg-gray-100 focus:text-teal-700 transition duration-150 ease-in-out" role="menuitem" to="/login">
                     Log in
-              </Link>
+                  </Link>
                 </div>
               </div>
             </div>
@@ -95,10 +94,10 @@ const LandingPage = () => {
             <div className="text-center">
               <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
                 Welcome to
-            <br className="xl:hidden" />
-                <span className="text-teal-500"> The Chess Centre</span>
+              <br className="xl:hidden" />
+                <span className="text-teal-500">The Chess Centre</span>
               </h2>
-              <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+              <p className="mt-5 sm:mt-3 sm:max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
                 It's time things changed around here. Brace yourselves! <span role="img">🚀</span>
               </p>
               <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
