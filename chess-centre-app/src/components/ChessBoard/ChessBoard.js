6002e55b6f42462d54d0e252fd3ae83979d0a5fe
Matthew D Webb <matt.d.webb@icloud.com>
2021-03-23 17:17:18 +0000

    feat: add pgn viewer to member profile

---
+++ chess-centre-app/src/components/ChessBoard/ChessBoard.js
@@ -0,0 +1,29 @@
+import React, { useLayoutEffect } from "react";
+import Children from "react-children-utilities";
+import * as uuid from "uuid";
+import { pgnView } from "@mliebelt/pgn-viewer";
+
+function PGNViewer(props) {
+  const { layout }  = props;
+  const gameDecription = Children.onlyText(props.children);
+  const id = "board-" + uuid.v4();
+
+  useLayoutEffect(() => {
+    pgnView(id, {
+      pgn: gameDecription,
+      mode: "view",
+      theme: "informator",
+      layout: layout,
+      hideMovesBefore: true,
+      locale: "en",
+      showResult: true,
+      boardSize: "280",
+      showFen: false,
+      pieceStyle: "alpha",
+    });
+  });
+
+  return <div id={id}></div>;
+}
+
+export default PGNViewer;
--- chess-centre-app/src/components/MemberProfile/MemberProfile.js
+++ chess-centre-app/src/components/MemberProfile/MemberProfile.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Chessboard from 'reactjs-chessboard';
+import PGNViewer from "../ChessBoard/ChessBoard";
 
 function MemberProfile() {
   const { memberId } = useParams();
@@ -66,17 +66,48 @@ function MemberProfile() {
           </div>
         </div>
       </div>
-      <div className="m-1 mt-2 sm:m-4 sm:mt-10">
+      <div className="m-1 mt-2 sm:m-4">
         <div className="">
-        <Chessboard
-        blackSquareColour="#5499ab"
-        whiteSquareColour="white"
-        width={320}
-        style={{
-          border: '2px solid lightgrey',
-        }}
-        orientation="b"
-      />
+        <div className="relative h-full max-w-7xl mx-auto items-center place-content-center content-center place-items-center">
+        
+
+        <div class="hidden sm:block">
+
+          <PGNViewer layout={"left"}>
+            [Event "F/S Return Match"] [Site "Belgrade"] [Date "1992.11.04"] [Round
+            "29"] [White "Fischer, Robert J."] [Black "Spassky, Boris V."] [Result
+            "1/2-1/2"] 1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 4. Ba4 Nf6 5. O-O Be7 6. Re1 b5
+            7. Bb3 d6 8. c3 O-O 9. h3 Nb8 10. d4 Nbd7 11. c4 c6 12. cxb5 axb5 13.
+            Nc3 Bb7 14. Bg5 b4 15. Nb1 h6 16. Bh4 c5 17. dxe5 Nxe4 18. Bxe7 Qxe7 19.
+            exd6 Qf6 20. Nbd2 Nxd6 21. Nc4 Nxc4 22. Bxc4 Nb6 23. Ne5 Rae8 24. Bxf7+
+            Rxf7 25. Nxf7 Rxe1+ 26. Qxe1 Kxf7 27. Qe3 Qg5 28. Qxg5 hxg5 29. b3 Ke6
+            30. a3 Kd6 31. axb4 cxb4 32. Ra5 Nd5 33. f3 Bc8 34. Kf2 Bf5 35. Ra7 g6
+            36. Ra6+ Kc5 37. Ke1 Nf4 38. g3 Nxh3 39. Kd2 Kb5 40. Rd6 Kc5 41. Ra6 Nf2
+            42. g4 Bd3 43. Re6 1/2-1/2
+          </PGNViewer>
+
+        </div>
+
+        <div class="block sm:hidden ml-4">
+
+            <PGNViewer layout={"top"}>
+              [Event "F/S Return Match"] [Site "Belgrade"] [Date "1992.11.04"] [Round
+              "29"] [White "Fischer, Robert J."] [Black "Spassky, Boris V."] [Result
+              "1/2-1/2"] 1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 4. Ba4 Nf6 5. O-O Be7 6. Re1 b5
+              7. Bb3 d6 8. c3 O-O 9. h3 Nb8 10. d4 Nbd7 11. c4 c6 12. cxb5 axb5 13.
+              Nc3 Bb7 14. Bg5 b4 15. Nb1 h6 16. Bh4 c5 17. dxe5 Nxe4 18. Bxe7 Qxe7 19.
+              exd6 Qf6 20. Nbd2 Nxd6 21. Nc4 Nxc4 22. Bxc4 Nb6 23. Ne5 Rae8 24. Bxf7+
+              Rxf7 25. Nxf7 Rxe1+ 26. Qxe1 Kxf7 27. Qe3 Qg5 28. Qxg5 hxg5 29. b3 Ke6
+              30. a3 Kd6 31. axb4 cxb4 32. Ra5 Nd5 33. f3 Bc8 34. Kf2 Bf5 35. Ra7 g6
+              36. Ra6+ Kc5 37. Ke1 Nf4 38. g3 Nxh3 39. Kd2 Kb5 40. Rd6 Kc5 41. Ra6 Nf2
+              42. g4 Bd3 43. Re6 1/2-1/2
+            </PGNViewer>
+
+        </div>
+
+
+
+      </div>
         </div>
           
       </div>
