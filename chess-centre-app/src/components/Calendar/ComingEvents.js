ea397a3f846dbf22338b54a6745abc52e6daf005
Adam Elmore <elmore.adam@gmail.com>
2021-04-14 06:28:34 -0500

    feat: data driven events

--- chess-centre-app/src/components/Calendar/ComingEvents.js
+++ chess-centre-app/src/components/Calendar/ComingEvents.js
@@ -1,29 +1,50 @@
-import React, { useState } from "react";
-import getEventDetails from "./Calendar";
+import React, { useEffect, useState } from "react";
+import { DataStore } from "@aws-amplify/datastore";
 import { CalendarIcon, ClockIcon } from "../../icons";
+import { Event } from "../../models";
 
-function Card({ id, name, description, time, url, textDate, color }) {
+function formatDate(date) {
+  return new Date(date).toLocaleDateString("en-GB", {
+    weekday: "short",
+    day: "numeric",
+    month: "long",
+  });
+}
+
+function Card({ event }) {
   return (
     <article
-      className={color + " p-6 shadow-2xl flex flex-col rounded-xl"}
+      className={event.type.color + " p-6 shadow-2xl flex flex-col rounded-xl"}
     >
       <header>
         <h3 className="h4 font-red-hat-display mb-1">
-          {name}
+          {event.name || event.type.name}
         </h3>
       </header>
       <div className="text-gray-600 flex-grow">
         <div>
-        <p className="sm:inline mr-1 text-sm text-teal-700 font-bold"><CalendarIcon className="w-4 h-4 inline mr-1" /><span className="inline">{textDate}</span> </p>
-        { time ? <p className="sm:inline text-sm text-teal-700"><ClockIcon className="w-4 h-4 inline mr-1" /><span className="inline">{time}</span> </p> : null }
+          <p className="sm:inline mr-1 text-sm text-teal-700 font-bold">
+            <CalendarIcon className="w-4 h-4 inline mr-1" />
+            <span className="inline">{`${formatDate(event.startDate)}${
+              event.endDate ? ` - ${formatDate(event.endDate)}` : ""
+            }`}</span>{" "}
+          </p>
+          {event.time || event.type.time ? (
+            <p className="sm:inline text-sm text-teal-700">
+              <ClockIcon className="w-4 h-4 inline mr-1" />
+              <span className="inline">
+                {event.time || event.type.time}
+              </span>{" "}
+            </p>
+          ) : null}
         </div>
 
-        <p className="mr-1">{description}</p>
+        <p className="mr-1">{event.description || event.type.description}</p>
       </div>
-      {url ? (
+      {event.type.url ? (
         <a
           className="inline-flex items-center font-medium text-teal-500 hover:underline mt-2"
-          href={ id ? `${url}/${id}` : url }
+          href={`${event.type.url}/${event.id}`}
         >
           <span>Find out more</span>
           <svg
@@ -43,9 +64,23 @@ function Card({ id, name, description, time, url, textDate, color }) {
 }
 
 function Timeline() {
-  const [month, setMonth] = useState("May");
+  const [month, setMonth] = useState(4);
+  const [events, setEvents] = useState([]);
 
-  const months = ["May", "June", "July", "August"];
+  const months = [4, 5, 6, 7];
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
+  async function fetchEvents() {
+    try {
+      const events = await DataStore.query(Event);
+      setEvents(events);
+    } catch (err) {
+      console.log("error fetching events");
+    }
+  }
 
   return (
     <section>
@@ -54,9 +89,7 @@ function Timeline() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-10">
             <h2 className="h2 font-red-hat-display mb-4">Our next Events</h2>
-            <p className="text-xl text-gray-600">
-              See what's coming up
-            </p>
+            <p className="text-xl text-gray-600">See what's coming up</p>
           </div>
 
           {/* Section content */}
@@ -70,17 +103,23 @@ function Timeline() {
                 >
                   <div className="absolute inset-0 w-0.5 h-full bg-gray-300"></div>
                 </div>
-                { months.map((m, i) => {
+                {months.map((m, i) => {
                   const isEven = i % 2 === 0;
                   return (
-                    <button key={i}
+                    <button
+                      key={i}
                       className="flex items-center justify-between font-medium text-gray-500 w-20 py-3 pr-2 text-left"
                       onClick={() => setMonth(m)}
                     >
-                      <span className="block w-12 truncate">{m}</span>
+                      <span className="block w-12 truncate">
+                        {new Date(2000, m, 1).toLocaleString("default", {
+                          month: "long",
+                        })}
+                      </span>
                       <span
                         className={`block w-3.5 h-3.5 bg-gray-400 border-2 border-white rounded-full ${
-                          month === m && (isEven ? "bg-teal-brand " : "bg-orange-brand ") 
+                          month === m &&
+                          (isEven ? "bg-teal-brand " : "bg-orange-brand ")
                         }`}
                       ></span>
                     </button>
@@ -90,11 +129,16 @@ function Timeline() {
 
               {months.map((m, i) => {
                 return (
-                  <div key={i} className={`flex-grow ${month !== m && "hidden"}`}>
+                  <div
+                    key={i}
+                    className={`flex-grow ${month !== m && "hidden"}`}
+                  >
                     <div className="grid md:grid-cols-2 gap-4 sm:gap-6">
-                      {getEventDetails(m).map((d, index) => (
-                        <Card key={index} {...d} />
-                      ))}
+                      {events
+                        .filter((d) => new Date(d.startDate).getMonth() === m)
+                        .map((d, index) => (
+                          <Card key={index} event={d} />
+                        ))}
                     </div>
                   </div>
                 );
