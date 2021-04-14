9497910ab0ee58c4bf4194b65f4067da61c34593
Adam Elmore <elmore.adam@gmail.com>
2021-04-14 06:30:51 -0500

    chore: cleanup unused code

--- chess-centre-app/src/components/Calendar/Calendar.js
+++
@@ -1,6 +0,0 @@
-import { eventDetails } from "../../api/mock.events";
-
-const getEventDetails = (month) =>
-  eventDetails.filter((d) => d.textDate.includes(month));
-
-export default getEventDetails;
