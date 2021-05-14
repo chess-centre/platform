5c6bed78a8971ff55b5232749eba3c6179fccd72
Matthew D Webb <matt.d.webb@icloud.com>
2021-05-14 11:20:36 +0100

    fix: css button clash

--- chess-centre-app/src/pages/App/Members.js
+++ chess-centre-app/src/pages/App/Members.js
@@ -115,7 +115,7 @@ function Members() {
                       </p>
                     </li>
                   </ul>
-                  <div className="hidden text-center">
+                  <div className="text-center">
                     <span
                       className="text-teal-600 text-xs cursor-pointer"
                       onClick={() => setShowSneakPeak(true)}
