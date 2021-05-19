09fe6f842f1c3da9f1261d1227ded7a6bb535d39
Matthew D Webb <matt.d.webb@icloud.com>
2021-05-19 23:38:04 +0100

    fix: junior rapidplay non-user

--- chess-centre-app/src/pages/Events/JuniorRapidplay.js
+++ chess-centre-app/src/pages/Events/JuniorRapidplay.js
@@ -23,7 +23,9 @@ export default function JuniorRapidplayEvent() {
             defaultPrice
           }
         }},
-      } = await API.graphql({ query: getEvent, variables: { id } });
+      } = await API.graphql({ query: getEvent, variables: { id } }).catch(e => {
+        console.log(e);
+      });
       setStartDate(startDate);
       setDefaultPrice(defaultPrice);
     }
