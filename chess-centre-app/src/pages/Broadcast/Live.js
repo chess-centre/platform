1a21bff37457fcee2887ec0aa450d9f0e79f6ad9
Matt D. Webb <36933715+matt-d-webb@users.noreply.github.com>
2021-05-22 10:27:14 +0100

    Update Live.js

--- chess-centre-app/src/pages/Broadcast/Live.js
+++ chess-centre-app/src/pages/Broadcast/Live.js
@@ -3,7 +3,7 @@ import LandingNav from "../../components/Navigation/LandingNav";
 import FooterLanding from "../../components/Footer/LandingFooter";
 
 const Live = () => {
-  const url = "https://view.livechesscloud.com/#da3bbd37-ca97-4196-b0c5-56e6cf004054";
+  const url = "https://view.livechesscloud.com#d9c89d4b-c027-46df-96c3-20e876b1a65c";
   return (
     <div>
       <div className="relative z-10 pt-6 pb-6 sm:pb-6 md:pb-6 lg:pb-6 xl:pb-6">
