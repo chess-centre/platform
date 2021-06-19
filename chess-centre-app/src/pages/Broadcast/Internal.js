66e328f95ed620757ec56b4f342a11a86a3ffd04
Matthew Webb <matt.d.webb@icloud.com>
2021-06-19 09:00:30 +0100

    fix local host

--- chess-centre-app/src/pages/Broadcast/Internal.js
+++ chess-centre-app/src/pages/Broadcast/Internal.js
@@ -5,18 +5,6 @@ const Internal = () => {
   return (
 
     <div className="grid grid-rows-4 grid-flow-col gap-4 px-10 py-10 h-screen">
-      <div className="row-span-4 col-span-4 bg-gray-100 rounded-lg shadow-xs overflow-hidden">
-        <div>
-          <div className="aspect-w-16 aspect-h-11">
-            <iframe
-              title="Live Games"
-              frameBorder="0"
-              allowFullScreen
-              src={"https://192.168.1.248:1982/liveviewer/index.html"}
-            />
-          </div>
-        </div>
-      </div>
       <div className="col-span-1 row-span-3 bg-gray-100 rounded-lg shadow-xs p-4">
         <div className="text-center">
           <h2 className="text-2xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
