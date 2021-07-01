63040518d888582fd0413cbab9aab0e1f33a855c
Matthew D Webb <matt.d.webb@icloud.com>
2021-07-01 23:11:14 +0100

    feat: prepare live viewer for cms

--- chess-centre-app/src/pages/Broadcast/LiveResults.js
+++ chess-centre-app/src/pages/Broadcast/LiveResults.js
@@ -1,33 +1,9 @@
 import React from "react";
 import API from "@aws-amplify/api";
-
-const listResults = /* GraphQL */ `
-  query ListResults(
-    $filter: ModelResultFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listResults(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        pairings
-        results
-        players
-        eventID
-        _version
-        _deleted
-        _lastChangedAt
-        createdAt
-        updatedAt
-      }
-      nextToken
-      startedAt
-    }
-  }
-`;
+import { getResult } from "../../graphql/queries";
+import { useParams } from "react-router-dom";
 
 const resultCheck = (pairings, players, results) => {
-
   const resultBySeed = [];
   pairings.forEach(({ round, pairings }) => {
     const pairingResults = results.find((r) => r.round === round).pairResults;
@@ -52,27 +28,33 @@ const resultCheck = (pairings, players, results) => {
       );
     });
   });
+  console.log(resultBySeed)
   const roundByRound = resultBySeed.reduce((player, { seed, result }) => {
-    console.log("NAME", player.name);
     if (!player[seed]) {
       const p = players.find((p) => p.seed === seed);
-      player[seed] = {
-        rounds: [result],
-        total: result || 0,
-        name: p.name,
-        rating: p.rating,
-      };
+      if(p) {
+        player[seed] = {
+          rounds: [result],
+          total: result || 0,
+          name: p?.name,
+          rating: p?.rating,
+        };
+      }
     } else {
       player[seed].rounds.push(result);
       player[seed].total += result || 0;
     }
     return player;
   }, {});
+
   return { resultBySeed, roundByRound };
 };
 
 export const CurrentStandings = ({ results, players, pairings }) => {
+
+
   const { roundByRound } = resultCheck(pairings, players, results);
+ 
 
   return (
     <div>
@@ -82,6 +64,12 @@ export const CurrentStandings = ({ results, players, pairings }) => {
       <table className="w-full divide-y divide-gray-200 dark:divide-gray-700 border-gray-300 border dark:border-gray-700 shadow">
         <thead className="bg-gray-50 dark:bg-gray-800">
           <tr>
+            <th
+              scope="col"
+              className="text-md font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider"
+            >
+              Pos.
+            </th>
             <th
               scope="col"
               className="px-4 sm:px-6 py-3 text-left text-md font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider"
@@ -108,7 +96,10 @@ export const CurrentStandings = ({ results, players, pairings }) => {
             .map((data, key) => {
               return (
                 <tr key={key} className="bg-white dark:bg-gray-800">
-                  <td className="px-2 pl-4 sm:px-4 py-2 whitespace-nowrap text-md font-medium text-gray-900 dark:text-gray-300">
+                  <td className="whitespace-nowrap text-md font-medium text-gray-900 dark:text-gray-300">
+                    {key + 1}
+                  </td>
+                  <td className="px-2 pl-4 sm:px-4 py-2 text-left whitespace-nowrap text-md font-medium text-gray-900 dark:text-gray-300">
                     {data.name}{" "}
                     <span className="font-thin">
                       ({data.rating ? data.rating : "unrated"})
@@ -144,30 +135,30 @@ export const PairsTable = ({ format, players, results }) => {
   return (
     <div>
       <table className="w-full divide-y divide-gray-200 dark:divide-gray-700 table-auto border-gray-300 dark:border-gray-700 border shadow">
-        <thead className="bg-gray-50 dark:bg-gray-800">
+        <thead className="bg-teal-800">
           <tr>
             <th
               scope="col"
-              className="px-2 sm:px-4 py-2 text-center text-md font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider"
+              className="px-2 sm:px-4 py-2 text-center text-md font-medium text-gray-100 dark:text-gray-300 uppercase tracking-wider"
             >
               White
             </th>
             <th
               scope="col"
-              className="px-4 sm:px-6 py-3 text-center text-md font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider"
+              className="px-4 sm:px-6 py-3 text-center text-md font-medium text-gray-100 dark:text-gray-300 uppercase tracking-wider"
             >
               Vs
             </th>
             <th
               scope="col"
-              className="px-4 sm:px-6 py-3 text-center text-md font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider"
+              className="px-4 sm:px-6 py-3 text-center text-md font-medium text-gray-100 dark:text-gray-300 uppercase tracking-wider"
             >
               Black
             </th>
           </tr>
         </thead>
         <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700">
-          <tr className="bg-white dark:bg-gray-800">
+          <tr className="bg-gray-200">
             {/* using colSpan=3 here means the header VS doesn't align center with the Round */}
             <td className="px-4 sm:px-6 py-3 text-center text-sm font-medium text-gray-900 dark:text-gray-300"></td>
             <td className="px-4 sm:px-6 py-1 text-center text-xs font-medium text-gray-900 dark:text-gray-300">
@@ -191,8 +182,9 @@ export const PairsTable = ({ format, players, results }) => {
                 </td>
                 <td className="px-4 sm:px-6 py-4 whitespace-nowrap text-center text-md text-gray-500 dark:text-gray-300 border-l-2 border-r-2 border-gray-100 dark:border-gray-700">
                   {white || black
-                    ? `${white === 0.5 ? "½" : white} - ${black === 0.5 ? "½" : black
-                    }`
+                    ? `${white === 0.5 ? "½" : white} - ${
+                        black === 0.5 ? "½" : black
+                      }`
                     : "? - ?"}
                 </td>
                 <td className="px-2 pl-4 sm:px-4 py-2 whitespace-nowrap text-center text-md font-medium text-gray-700 dark:text-gray-300">
@@ -210,247 +202,87 @@ export const PairsTable = ({ format, players, results }) => {
   );
 };
 
-const p = [
-  {
-    seed: 1,
-    id: 1,
-    name: "Matthew Webb",
-    rating: 2553
-  },
-  {
-    seed: 2,
-    id: 2,
-    name: "Bernard Chan",
-    rating: 2095
-  },
-  {
-    seed: 3,
-    id: 3,
-    name: "Andrew Wainwright",
-    rating: 2010
-  },
-  {
-    seed: 4,
-    id: 4,
-    name: "Tim Hilton",
-    rating: 2005
-  },
-  {
-    seed: 5,
-    id: 5,
-    name: "Sam Davies",
-    rating: 1960
-  },
-  {
-    seed: 6,
-    id: 6,
-    name: "Gawain Ako",
-    rating: 1942
-  },
-  {
-    seed: 7,
-    id: 7,
-    name: "Kevin Winter",
-    rating: 1788
-  },
-  {
-    seed: 8,
-    id: 8,
-    name: "Martin Gawne",
-    rating: 1690
-  },
-  {
-    seed: 9,
-    id: 9,
-    name: "Gary Corcoran",
-    rating: 1668
-  },
-  {
-    seed: 10,
-    id: 10,
-    name: "Jiping Li",
-    rating: 1580
-  }
-];
-
 export const Internal = () => {
-  const [players, setPlayers] = React.useState(p);
-  const [pairings, setPairings] = React.useState([
-    {
-      round: 1,
-      pairings: [
-        [1,6],
-        [7,2],
-        [3, 8],
-        [9, 4],
-        [5, 10]
-      ],
-    },
-    {
-      round: 2,
-      pairings: [
-        [],
-        [],
-        [],
-        [],
-        []
-      ],
-    },
-    {
-      round: 3,
-      pairings: [
-        [],
-        [],
-        [],
-        [],
-        []
-      ],
-    },
-    {
-      round: 4,
-      pairings: [
-        [],
-        [],
-        [],
-        [],
-        []
-      ],
-    },
-    {
-      round: 5,
-      pairings: [
-        [],
-        [],
-        [],
-        [],
-        []
-      ],
-    },
-  ]);
-  const [resultDetails, setResultDetails] = React.useState(
-    [
-      {
-        round: 1,
-        pairResults: [[], [], [], [], []]
-      },
-      {
-        round: 2,
-        pairResults: [[], [], [], [], []]
-      },
-      {
-        round: 3,
-        pairResults: [[], [], [], [], []]
-      },
-      {
-        round: 4,
-        pairResults: [[], [], [], [], []]
-      },
-      {
-        round: 5,
-        pairResults: [[], [], [], [], []]
-      }
-    ]
-  );
+  const { id } = useParams();
+  const [title, setTitle] = React.useState("");
+  const [players, setPlayers] = React.useState([]);
+  const [pairings, setPairings] = React.useState([]);
+  const [resultDetails, setResultDetails] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
     setIsLoading(true);
-    // const fetchResults = async () => {
-    //   const {
-    //     data: {
-    //       listResults: { items: results },
-    //     },
-    //   } = await API.graphql({
-    //     query: listResults,
-    //   });
-    //   try {
-    //     const parsedResult = JSON.parse(results[0].results);
-    //     const parsedPlayer = JSON.parse(results[0].players);
-    //     const parsedPairings = JSON.parse(results[0].pairings);
-    //     setResultDetails(parsedResult);
-    //     setPlayers([
-    //       ...parsedPlayer.map((m, i) => {
-    //         m.seed = i + 1;
-    //         return m;
-    //       }),
-    //     ]);
-    //     setPairings(parsedPairings);
-    //     console.log("parsedPairings", pairings[0].round);
-    //   } catch (error) {
-    //     console.log("Something exploded!", error);
-    //   }
-    // };
-    // fetchResults();
+    const fetchResults = async () => {
+      const {
+        data: { getResult: result },
+      } = await API.graphql({
+        query: getResult,
+        variables: { id },
+      });
+
+      try {
+        const parsedResult = JSON.parse(result.results);
+        const parsedPlayer = JSON.parse(result.players);
+        const parsedPairings = JSON.parse(result.pairings);
+        setResultDetails(parsedResult);
+        setPlayers([
+          ...parsedPlayer.map((m, i) => {
+            m.seed = i + 1;
+            return m;
+          }),
+        ]);
+        setPairings(parsedPairings);
+        setTitle(result.name);
+      } catch (error) {
+        setIsLoading(false);
+        setIsError(true);
+        console.log("Something exploded!", error);
+      }
+    };
+    fetchResults();
     setIsLoading(false);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div className="grid gap-4 px-2 py-2 h-screen">
-      {isLoading ? (
+      {isError ? (
+        "Oops. Something went wrong!"
+      ) : isLoading ? (
         "Loading..."
       ) : (
-        <div className="col-span-2 bg-gray-100 rounded-lg shadow-xs">
+        <div className="col-span-2 rounded-lg shadow-xs">
           <h1 className="text-2xl tracking-tight leading-10 font-extrabold text-gray-900">
-            June Rapidplay
+            {title}
           </h1>
           <div className="mb-4">
-            {Array.isArray(pairings) && pairings.length ? (
-              <CurrentStandings
-                results={resultDetails}
-                players={players}
-                pairings={pairings}
-              ></CurrentStandings>
-            ) : (
-              "Loading"
-            )}
+            <CurrentStandings
+              results={resultDetails}
+              players={players}
+              pairings={pairings}
+            ></CurrentStandings>
           </div>
           <div className="mb-2">
-            {Array.isArray(pairings) && pairings.length ? (
-              <div className="grid grid-cols-2 gap-2">
-                {/* <div>
-                  <div className="pb-2">
-                    <PairsTable
-                      format={pairings[0]}
-                      players={players}
-                      results={resultDetails}
-                    />
-                  </div>
-                  <div className="pb-2">
-                    <PairsTable
-                      format={pairings[1]}
-                      players={players}
-                      results={resultDetails}
-                    />
-                  </div>
-                  <div className="pb-2">
-                    <PairsTable
-                      format={pairings[2]}
-                      players={players}
-                      results={resultDetails}
-                    />
-                  </div>
+            <div className="grid grid-cols-2 gap-2">
+              <div>
+                <div className="pb-2">
+                  {pairings.map((pairing) => {
+                    if (pairing.pairings[0].length > 0) {
+                      return (
+                        <PairsTable
+                          format={pairing}
+                          players={players}
+                          results={resultDetails}
+                        />
+                      );
+                    } else {
+                      return <></>;
+                    }
+                  })}
                 </div>
-                <div>
-                  <div className="pb-2">
-                    <PairsTable
-                      format={pairings[3]}
-                      players={players}
-                      results={resultDetails}
-                    />
-                  </div>
-                  <div className="pb-2">
-                    <PairsTable
-                      format={pairings[4]}
-                      players={players}
-                      results={resultDetails}
-                    />
-                  </div>
-                </div> */}
               </div>
-            ) : (
-              "Loading"
-            )}
+            </div>
           </div>
         </div>
       )}
