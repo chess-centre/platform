31311b006c504f797bc9a9db9d356c72b498d357
Matthew D Webb <matt.d.webb@icloud.com>
2021-07-02 22:01:58 +0100

    feat: added event entries table to dashboard

--- chess-centre-app/src/components/Chart/ChartCard.js
+++ chess-centre-app/src/components/Chart/ChartCard.js
@@ -2,11 +2,14 @@ import React from "react";
 
 function Chart({ children, title }) {
   return (
-    <div className="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+    <div className="relative min-w-0 p-4 bg-white rounded-lg shadow dark:bg-gray-800">
       <p className="mb-4 font-semibold text-gray-800 dark:text-gray-300">
         {title}
       </p>
+      <div className="mb-2">
       {children}
+      </div>
+      <div className="absolute rounded-b-lg bottom-0 inset-x-0 bg-gray-50 dark:bg-gray-800 px-4 py-2 sm:px-6 border-t border-gray-100"></div>
     </div>
   );
 }
