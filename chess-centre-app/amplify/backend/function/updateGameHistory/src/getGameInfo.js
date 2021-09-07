fc62ee80bd0bfd37411a51ae9a14ca6f872fd9b1
Matthew Webb <matt.d.webb@icloud.com>
2021-09-07 17:48:37 +0100

    feat: game history dashboard updates

--- chess-centre-app/amplify/backend/function/updateGameHistory/src/getGameInfo.js
+++ chess-centre-app/amplify/backend/function/updateGameHistory/src/getGameInfo.js
@@ -1,4 +1,3 @@
-
 const AWS = require("aws-sdk");
 const { API_GAMETABLE_NAME } = process.env;
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -38,7 +37,23 @@ exports.getGameInfo = async (memberId) => {
     dynamodb.query(blackParams).promise(),
   ]);
 
-  return [...asWhitePlayer, ...asBlackPlayer];
+  return prepareGameData([...asWhitePlayer, ...asBlackPlayer]);
+};
 
-
-} 
+function prepareGameData(games) {
+  // This structures the data so I can be passed to the users dashboard:
+  return games.reduce((prev, { date, type }) => {
+    const month = date ? new Date(date).toLocaleString('default', { month: 'long' }) : '';
+    const exists = prev.find(g => g.month === month);
+    if (exists) {
+      const index = prev.findIndex(g => g.month === month);
+      prev[index][type] ? prev[index][type] += 1 : prev[index][type] = 0;
+      return [...prev];
+    } else {
+      return [...prev, {
+        month,
+        [type]: 1
+      }];
+    }
+  }, []);
+}
--- chess-centre-app/amplify/backend/function/updateGameHistory/src/index.js
+++ chess-centre-app/amplify/backend/function/updateGameHistory/src/index.js
@@ -1,4 +1,3 @@
-
 const AWS = require("aws-sdk");
 const { API_MEMBERTABLE_NAME } = process.env;
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -6,25 +5,38 @@ const getGameInfo = require("./getGameInfo").getGameInfo;
 
 exports.handler = async (event) => {
 
-
-    try {
+    try {        
+        const {
+            Items: members
+        } = await dynamodb.scan({
+            TableName: API_MEMBERTABLE_NAME,
+            FilterExpression: "attribute_exists(ecfId)"
+        }).promise();
         
-        // const {
-        //     Items
-        // } = await dynamodb.scan({
-        //     TableName: API_MEMBERTABLE_NAME,
-        //     FilterExpression: "attribute_exists(ecfId)"
-        // }).promise();
-
-        const games = await getGameInfo("266e23de-3ead-4afd-8115-c34a3bcb2e7b");
-        console.log(games);
+        const updated = members.map(async member => {
+            const games = await getGameInfo(member.id);
+            console.log(games);
+            const params = {
+                TableName: API_MEMBERTABLE_NAME,
+                Key: {
+                    id: member.id
+                },
+                UpdateExpression: "set gameInfo=:info",
+                ExpressionAttributeValues: {
+                    ":info": games
+                },
+                ReturnValues: "UPDATED_NEW"
+            };
+            const response = await dynamodb.update(params).promise();
+            console.log(`Saving game data for ${member.name}`);
+            return response;
+        });
+        const complete = await Promise.all(updated);
+        console.log(`All ${complete.length} member game info updates complete.`);
 
     } catch (error) {
-        
-    }
-
-
-
+        console.log(error);
+    };
     const response = {
         statusCode: 200,
         headers: {
