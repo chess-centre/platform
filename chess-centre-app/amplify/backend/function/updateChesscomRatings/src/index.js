9eee57b4f3bb069e852d9e87c51eb9a7ff26688e
Matthew D Webb <matt.d.webb@icloud.com>
2022-01-02 21:01:16 +0000

    feat: add online rating updating lambda services

---
+++ chess-centre-app/amplify/backend/function/updateChesscomRatings/src/index.js
@@ -0,0 +1,53 @@
+
+const getMemberHandles = require("./getMemberHandles").getMemberHandles;
+const getChesscomInfo = require("./getChesscomInfo").getChesscomInfo;
+const updateMemberRecord = require("./updateMemberRecord").updateMemberRecord;
+
+exports.handler = async (event) => {
+
+    const memberHandles = await getMemberHandles();
+
+    if(!memberHandles) {
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*"
+            }, 
+            body: JSON.stringify({ error: "No chess.com handles found!"}),
+        };
+    }
+
+    const chesscomDate = await Promise.all(memberHandles.map(async m => {
+        const info = await getChesscomInfo(m.chesscomUsername);
+        if(info) {
+            console.log("POST: updating record for", m.name);
+            await updateMemberRecord(m.id, info);
+            return true;
+        } else {
+            console.log("unable to update record for", m.name);
+            return false;
+        }
+    }));
+
+    if(chesscomDate) {
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*"
+            }, 
+            body: JSON.stringify("done"),
+        };
+    } else {
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*"
+            }, 
+            body: JSON.stringify({ error: "Something went wrong!"}),
+        };
+    }
+};
+
---
+++ chess-centre-app/amplify/backend/function/updateLichessPlayerRatings/src/getMemberHandles.js
@@ -0,0 +1,15 @@
+const AWS = require("aws-sdk");
+const { MEMBERTABLE_NAME } = process.env;
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+exports.getMemberHandles = async () => {
+
+    const {
+      Items: members
+    } = await dynamodb.scan({
+        TableName: MEMBERTABLE_NAME,
+        FilterExpression: "attribute_exists(liChessUsername)"
+    }).promise();
+
+    return members;
+}
