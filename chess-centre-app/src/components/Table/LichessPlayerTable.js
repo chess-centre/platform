0b1eb98c33ad9f0e8965507f578602b7f45b352f
Matthew D Webb <matt.d.webb@icloud.com>
2022-01-10 23:16:12 +0000

    feat: lichess daily change updates

--- chess-centre-app/src/components/Table/LichessPlayerTable.js
+++ chess-centre-app/src/components/Table/LichessPlayerTable.js
@@ -2,8 +2,20 @@ import React from "react";
 import moment from "moment";
 import Table from "./index";
 
-
 export default function LichessPlayersTable({ players, colour }) {
+  const DiffArrow = (diff) => {
+    switch (diff) {
+      case 1:
+        return <i className="text-green-500 fas fa-caret-up mr-2"></i>;
+      case -1:
+        return <i className="text-red-700 fas fa-caret-down mr-2"></i>;
+      case 0:
+        return <i className="text-gray-200 far fa-caret-right mr-2"></i>;
+      default:
+        return <></>;
+    }
+  };
+
   const columns = React.useMemo(
     () => [
       {
@@ -12,12 +24,27 @@ export default function LichessPlayersTable({ players, colour }) {
         show: false,
       },
       {
+        Header: "bulletDiff",
+        accessor: "bulletDiff",
+        show: false,
+      },
+      {
+        Header: "blitzDiff",
+        accessor: "blitzDiff",
+        show: false,
+      },
+      {
+        Header: "rapidDiff",
+        accessor: "rapidDiff",
+        show: false,
+      },
+      {
         Header: "#",
         accessor: "rank",
       },
       {
         Header: "Name",
-        accessor: "name"
+        accessor: "name",
       },
       {
         Header: "Handle",
@@ -27,9 +54,15 @@ export default function LichessPlayersTable({ players, colour }) {
         Header: "Total Game",
         accessor: "total",
         Cell: (props) => (
-          <div
-            className="font-medium text-sm"
-          >
+          <div className="font-medium text-sm text-center">{props.cell.value}</div>
+        ),
+      },
+      {
+        Header: "Bullet",
+        accessor: "lichessBullet",
+        Cell: (props) => (
+          <div className="text-sm text-gray-500">
+            {DiffArrow(props.row.values.bulletDiff)}
             {props.cell.value}
           </div>
         ),
@@ -37,30 +70,39 @@ export default function LichessPlayersTable({ players, colour }) {
       {
         Header: "Blitz",
         accessor: "lichessBlitz",
-      },
-      {
-        Header: "Bullet",
-        accessor: "lichessBullet",
+        Cell: (props) => (
+          <div className="text-sm text-gray-500">
+            {DiffArrow(props.row.values.blitzDiff)}
+            {props.cell.value}
+          </div>
+        ),
       },
       {
         Header: "Rapid",
         accessor: "lichessRapid",
+        Cell: (props) => (
+          <div className="text-sm text-gray-500">
+            {DiffArrow(props.row.values.rapidDiff)}
+            {props.cell.value}
+          </div>
+        ),
       },
       {
         Header: "Puzzle Rating",
         accessor: "puzzleRating",
+        Cell: (props) => (
+          <div className="font-medium text-sm text-center">{props.cell.value}</div>
+        ),
       },
       {
         Header: "Last Updated",
         accessor: "lastUpdated",
         Cell: (props) => (
-          <div
-            className="text-sm text-gray-500"
-          >
+          <div className="text-sm text-gray-500">
             {moment(props.cell.value).format("Do MMM @ HH:mm")}
           </div>
         ),
-      }
+      },
     ],
     []
   );
@@ -69,12 +111,14 @@ export default function LichessPlayersTable({ players, colour }) {
     <div className="min-h-screen bg-gray-50 text-gray-900">
       <main className="md:max-w-5xl">
         <div className="mt-6">
-          { players && <Table
-            columns={columns}
-            data={players}
-            searchPlaceholder="players..."
-            colour={colour}
-          /> }
+          {players && (
+            <Table
+              columns={columns}
+              data={players}
+              searchPlaceholder="players..."
+              colour={colour}
+            />
+          )}
         </div>
       </main>
     </div>
--- chess-centre-app/src/components/Table/index.js
+++ chess-centre-app/src/components/Table/index.js
@@ -176,7 +176,7 @@ function Table({ columns, data, searchPlaceholder = "games...", colour="bg-teal-
       columns,
       data,
       initialState: {
-        hiddenColumns: ["id", "liChessUrl", "eventId", "formArray"]
+        hiddenColumns: ["id", "liChessUrl", "eventId", "formArray", "bulletDiff", "blitzDiff", "rapidDiff"]
       }
     },
     useFilters, // useFilters!
