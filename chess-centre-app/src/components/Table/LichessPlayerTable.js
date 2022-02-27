73cdb524e9897398b67748f454d7a75b084db334
Matthew D Webb <matt.d.webb@icloud.com>
2022-02-27 18:12:48 +0000

    fix: player games formatting

--- chess-centre-app/src/components/Table/LichessPlayerTable.js
+++ chess-centre-app/src/components/Table/LichessPlayerTable.js
@@ -79,7 +79,7 @@ export default function LichessPlayersTable({ userId, players, colour, statuses 
         Header: () => <div className="mx-auto">Games</div>,
         accessor: "total",
         Cell: (props) => (
-          <div className="font-medium text-sm text-center">
+          <div className="font-medium text-sm text-right mr-5">
             {numberWithCommas(props.cell.value)}
           </div>
         ),
