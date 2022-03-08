3e04feebbfaaad21e04716b72734d884ae4daf22
matt-d-webb <matt.d.webb@icloud.com>
2022-03-08 23:48:24 +0000

    refactor: add the event information

--- chess-centre-app/src/pages/Broadcast/Rapidplay.js
+++ chess-centre-app/src/pages/Broadcast/Rapidplay.js
@@ -20,7 +20,7 @@ const Viewer = () => {
             {name}
           </h2>
           <div className="tracking-tight text-gray-100 text-md leading-none mt-3">
-            Welcome to the 1st dedicated, not for profit, chess center in the UK
+            Welcome to the 1st dedicated, not for profit, chess centre in the UK
           </div>
         </div>
         <div className="grid grid-cols-3 gap-10">
@@ -37,6 +37,23 @@ const Viewer = () => {
               />
             );
           })}
+        </div>
+        <div className="grid grid-cols-1 mt-4">
+          <div className=" bg-cool-gray-900 border-2 border-cool-gray-700">
+            <div className="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8 lg:py-6">
+              <div className="max-w-4xl mx-auto text-center">
+                <h2 className="text-3xl font-extrabold text-cool-gray-600 sm:text-2xl">
+                   Event Information
+                </h2>
+              </div>
+              <dl className="mt-4 text-center sm:max-w-3xl sm:mx-auto sm:grid sm:grid-cols-1 sm:gap-8">
+                <div className="flex flex-col">
+                  <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-400">Next Round</dt>
+                  <dd className="order-1 text-5xl font-extrabold text-white">10:00am</dd>
+                </div>
+              </dl>
+            </div>
+          </div>
         </div>
       </div>
     </div>
