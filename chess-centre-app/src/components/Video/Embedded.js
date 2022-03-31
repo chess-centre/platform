0ec302082c95504cf6bffd1269edd009a88dda55
matt-d-webb <matt.d.webb@icloud.com>
2022-03-31 17:38:25 +0100

    chore: typos and tweaks

---
+++ chess-centre-app/src/components/Video/Embedded.js
@@ -0,0 +1,15 @@
+export default () => {
+    return (<div className="relative max-w-6xl mx-auto px-4 sm:px-28" >
+        <div className="pt-4 pb-0 sm:py-10" >
+            <div className="aspect-w-16 aspect-h-9" >
+                <iframe
+                    src={`https://www.youtube.com/embed/FYG4Envbzro`}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    title="Embedded youtube"
+                />
+            </div>
+        </div>
+    </div>)
+}
--- chess-centre-app/src/pages/Home/Home.js
+++ chess-centre-app/src/pages/Home/Home.js
@@ -10,6 +10,7 @@ import DownloadPWA from "../../components/Quote/PWA";
 import GamesSignUp from "../../components/CTA/SignUp";
 import Integrations from "../../components/Integrations";
 import FestivalCTA from "../../components/Festival/HomePageCTA";
+import EmbeddedVideo from "../../components/Video/Embedded";
 
 
 import { useLiveGameState } from "../../context/LiveGameContext";
@@ -170,20 +171,7 @@ const Home = () => {
           </main>
         </div>
       </div>
-      <div className="relative max-w-6xl mx-auto px-4 sm:px-28">
-        <div className="pt-4 pb-0 sm:py-10">
-          <div className="aspect-w-16 aspect-h-9">
-            <iframe
-              src={`https://www.youtube.com/embed/FYG4Envbzro`}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              title="Embedded youtube"
-            />
-          </div>
-        </div>
-      </div>
-
+      <EmbeddedVideo />
       <FestivalCTA />
       <ComingEvents />
       <GamesSignUp />
