b12d48d2459c10ce2327a7aa83c13881541349bf
Matthew D Webb <matt.d.webb@icloud.com>
2022-04-07 18:09:02 +0100

    feat: add event type details

--- chess-centre-app/amplify/backend/function/getEvents/src/queries.js
+++ chess-centre-app/amplify/backend/function/getEvents/src/queries.js
@@ -1,14 +1,28 @@
 const AWS = require("aws-sdk");
 const dynamodb = new AWS.DynamoDB.DocumentClient();
-const { API_EVENT_TABLE_NAME } = process.env;
+const { API_EVENT_TABLE_NAME, API_EVENT_TYPE_TABLE_NAME } = process.env;
 
 exports.getLatestEvents = async () => {
+
+  const eventTypes = await dynamodb.scan({
+    TableName: API_EVENT_TYPE_TABLE_NAME,
+    ProjectionExpression: "#eventName, description, timeControl, eventType, maxEntries, #eventTime",
+    ExpressionAttributeNames: {
+      "#eventName": "name",
+      "#eventTime": "time",
+    }
+  }).promise();
 
   const items = await dynamodb.query({ 
     TableName: API_EVENT_TABLE_NAME, 
     IndexName: "eventsByActive",
     KeyConditionExpression: "#active = :active and #startDate >= :dateStr",
+    ProjectionExpression: "#id, #eventName, description, rounds, #eventTime, #eventType, startDate, endDate, isLive, isLiveUrl, complete, cancelled, entryCount, maxEntries",
     ExpressionAttributeNames: {
+      "#id": "id",
+      "#eventName": "name",
+      "#eventTime": "time",
+      "#eventType": "type",
       "#startDate": "startDate",
       "#active": "active"
     },
@@ -16,20 +30,12 @@ exports.getLatestEvents = async () => {
   }).promise();
 
   return items?.Items?.reduce((events, item) => {
-    return [...events, {
-      id: item.id,
-      name: item.name,
-      startDate: item.startDate,
-      endDate: item.endDate,
-      eventTypeId: item.eventTypeId,
-      rounds: item.rounds,
-      entryCount: item.entryCount,
-      maxEntries: item.maxEntries,
-      complete: item.complete,
-      updatedAt: item.updatedAt,
-      isLive: item.isLive,
-      cancelled: item.cancelled
-    }];
+    const eventType = eventTypes?.Items?.find(type => type.id === item.eventTypeId);
+    const currentEvent = {
+      ...eventType,
+      ...item
+    };
+    return [...events, currentEvent];
   }, []);
 }
 
