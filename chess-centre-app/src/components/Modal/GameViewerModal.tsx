1647c24a85614d543170bcbc053806e35c541a36
Matthew D Webb <matt.d.webb@icloud.com>
2022-10-21 13:22:29 +0100

    feat: add pgn download option to games and profile

--- chess-centre-app/src/components/Modal/GameViewerModal.tsx
+++ chess-centre-app/src/components/Modal/GameViewerModal.tsx
@@ -2,11 +2,22 @@ import React from "react";
 import { Dialog } from "@headlessui/react";
 import PGNViewer from "../ChessBoard/ChessBoard";
 
-export default function GameViewerModal({ open, closeModal, pgn }) {
+export default function GameViewerModal({ open, closeModal, pgn, fileName }) {
   let game = undefined;
 
   if (pgn) {
     game = pgn.replace(/["']/g, '"');
+  }
+
+  function downloadPGN(filename: string, data: string | undefined) {
+    if(!data) return;
+    const blob = new Blob([data], { type: "application/vnd.chess-pgn" });
+    const elem = window.document.createElement("a");
+    elem.href = window.URL.createObjectURL(blob);
+    elem.download = `${filename}.pgn`;
+    document.body.appendChild(elem);
+    elem.click();
+    document.body.removeChild(elem);
   }
 
   return (
@@ -21,14 +32,23 @@ export default function GameViewerModal({ open, closeModal, pgn }) {
                 </div>
               </div>
             </div>
-            <div className="mt-5 sm:mt-6">
+            <div className="mt-5 sm:mt-6 flex gap-4 sm:mx-8">
               <button
                 type="button"
-                className="inline-flex w-full justify-center rounded-md border border-transparent bg-teal-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 sm:text-sm"
+                className="inline-flex w-full justify-center rounded-md border border-gray-200 bg-gray-100 px-4 py-2 text-base font-medium text-black shadow-sm hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 sm:text-sm"
                 onClick={closeModal}
               >
-                Go back to games
+                Close
               </button>
+              {game && (
+                <button
+                  type="button"
+                  className="inline-flex w-full justify-center rounded-md border border-transparent bg-sky-700 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-sky-800 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 sm:text-sm"
+                  onClick={() => downloadPGN(fileName, game)}
+                >
+                  Download PGN
+                </button>
+              )}
             </div>
           </Dialog.Panel>
         </div>
