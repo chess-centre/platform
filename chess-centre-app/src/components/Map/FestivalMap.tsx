56d458f69a83e9e62058cb11f8a9ad699e02d0c9
Matthew D Webb <matt.d.webb@icloud.com>
2022-11-10 16:32:19 +0000

    fix: remove gmaps

--- chess-centre-app/src/components/Map/FestivalMap.tsx
+++ chess-centre-app/src/components/Map/FestivalMap.tsx
@@ -1,43 +1,43 @@
 import React from "react";
-import {
-  GoogleMap as GMap,
-  Marker,
-  withGoogleMap,
-  withScriptjs,
-} from "react-google-maps";
-import Logo from "../../assets/img/festival-map-pin.svg";
+// import {
+//   GoogleMap as GMap,
+//   Marker,
+//   withGoogleMap,
+//   withScriptjs,
+// } from "react-google-maps";
+// import Logo from "../../assets/img/festival-map-pin.svg";
 
-const GoogleMap = withScriptjs(
-  withGoogleMap((props) => (
-    <GMap
-      defaultZoom={17.5}
-      defaultCenter={{
-        lat: 53.92429665159249,
-        lng: -1.821591459945466,
-      }}
-    >
-      <Marker
-        position={{
-          lat: 53.92429665159249,
-          lng: -1.821591459945466,
-        }}
-        icon={{ url: Logo, width: 20, height: 20 }}
-      />
-    </GMap>
-  ))
-);
+// const GoogleMap = withScriptjs(
+//   withGoogleMap((props) => (
+//     <GMap
+//       defaultZoom={17.5}
+//       defaultCenter={{
+//         lat: 53.92429665159249,
+//         lng: -1.821591459945466,
+//       }}
+//     >
+//       <Marker
+//         position={{
+//           lat: 53.92429665159249,
+//           lng: -1.821591459945466,
+//         }}
+//         icon={{ url: Logo, width: 20, height: 20 }}
+//       />
+//     </GMap>
+//   ))
+// );
 
 const FestivalMap = () => {
   return (
     <div className="mt-6">
-      <GoogleMap
+      {/* <GoogleMap
         googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAnoxH8i-1VmO6KVg3VomCsv3l8tOfObOI"
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={
           <div style={{ height: `300px` }} />
         }
         mapElement={<div style={{ height: `100%` }} />}
-      />
+      /> */}
     </div>
   );
 };
--- chess-centre-app/src/components/Map/GoogleMap.tsx
+++ chess-centre-app/src/components/Map/GoogleMap.tsx
@@ -1,30 +1,30 @@
 import React from "react";
-import {
-  GoogleMap as GMap,
-  Marker,
-  withGoogleMap,
-  withScriptjs,
-} from "react-google-maps";
-import Logo from "../../assets/img/logo-map-pin-teal.svg";
+// import {
+//   GoogleMap as GMap,
+//   Marker,
+//   withGoogleMap,
+//   withScriptjs,
+// } from "react-google-maps";
+// import Logo from "../../assets/img/logo-map-pin-teal.svg";
 
-const GoogleMap = withScriptjs(
-  withGoogleMap((props) => (
-    <GMap
-      defaultZoom={16}
-      defaultCenter={{
-        lat: 53.925857457885456,
-        lng: -1.8227872999999994,
-      }}
-    >
-      <Marker
-        position={{
-          lat: 53.925857457885456,
-          lng: -1.8227872999999994
-        }}
-        icon={{ url: Logo, width: 20, height: 20 }} />
+// const GoogleMap = withScriptjs(
+//   withGoogleMap((props) => (
+//     <GMap
+//       defaultZoom={16}
+//       defaultCenter={{
+//         lat: 53.925857457885456,
+//         lng: -1.8227872999999994,
+//       }}
+//     >
+//       <Marker
+//         position={{
+//           lat: 53.925857457885456,
+//           lng: -1.8227872999999994
+//         }}
+//         icon={{ url: Logo, width: 20, height: 20 }} />
     
-    </GMap>
-  ))
-);
+//     </GMap>
+//   ))
+// );
 
-export default GoogleMap;
+// export default GoogleMap;
