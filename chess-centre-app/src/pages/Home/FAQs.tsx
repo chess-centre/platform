391257be51cee6f6cf2144e42722d0c3cab97d65
Matthew Webb <matt.d.webb@icloud.com>
2023-08-27 20:33:12 +0400

    feat: update for info home page only

--- chess-centre-app/src/pages/Home/FAQs.tsx
+++ chess-centre-app/src/pages/Home/FAQs.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 import queryString from "query-string";
 import LandingNav from "../../components/Navigation/LandingNav";
